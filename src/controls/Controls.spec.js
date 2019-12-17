d7100b7f0591d1a7451a3796ae1d8b4e2c81e30f
miriam-sullivan <maljayd42@gmail.com>
2019-12-17 12:38:25 -0600

    added everything for mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,30 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls'
+
+test("Render controls", () => {
+	render(<Controls />)
+})
+
+test("Matching snapshot created", () => {
+	expect(render(<Controls />)).toMatchSnapshot()
+})
+
+test("Check the default state of the controls", () => {
+	const { getByText } = render(<Controls />)
+	expect(getByText(/close gate/i));
+	expect(getByText(/lock gate/i));
+	expect(getByText(/lock gate/i).disabled).toBe(true);
+})
+
+test("Check for open gate control on the page when gate is locked", () => {
+	const { getByText } = render(<Controls locked={true} closed={true} />)
+	expect(getByText(/open gate/i).disabled).toBe(true);
+})
+
+test("Check for lock gate controls on the page when gate is open", () => {
+	const { getByText } = render(<Controls locked={false} closed={false} />)
+	expect(getByText(/close gate/i).disabled).toBe(false);
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+test("Render the Display", () => {
+  render(<Display />);
+});
+
+test("Match the current snap shot", () => {
+  expect(render(<Display />)).toMatchSnapshot();
+});
+
+test("Check the UI to make sure we are displaying open and unlocked", () => {
+  const { getByText } = render(<Display />);
+  expect(getByText(/open/i));
+  expect(getByText(/unlocked/i));
+});
+
+test("check that display will show closed and locked", () => {
+  const { getByText } = render(<Display closed={true} locked={true} />);
+  getByText(/closed/i);
+  getByText(/locked/i);
+});

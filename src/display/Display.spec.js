a8cabbd4b2e31eb15387c05d19c8ff4ebcb86b5f
miriam-sullivan <maljayd42@gmail.com>
2019-12-19 08:52:46 -0600

    did the class testings

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,25 +1,33 @@
-// Test away!
-
 import React from "react";
 import { render } from "@testing-library/react";
 import Display from "./Display";
+import "@testing-library/jest-dom/extend-expect";
 
-test("Render the Display", () => {
-  render(<Display />);
-});
-
-test("Match the current snap shot", () => {
-  expect(render(<Display />)).toMatchSnapshot();
-});
-
-test("Check the UI to make sure we are displaying open and unlocked", () => {
+test("Display Shows", () => {
   const { getByText } = render(<Display />);
-  expect(getByText(/open/i));
-  expect(getByText(/unlocked/i));
 });
 
-test("check that display will show closed and locked", () => {
+test("Display Gate State-Closed", () => {
   const { getByText } = render(<Display closed={true} locked={true} />);
   getByText(/closed/i);
   getByText(/locked/i);
+});
+
+test("Display Gate State-Closed", () => {
+  const { getByText } = render(<Display closed={false} locked={false} />);
+  getByText(/open/i);
+  getByText(/Unlock/);
+});
+
+test("Use red-led for locked or closed", () => {
+  const { getByText } = render(<Display closed={true} locked={true} />);
+  expect(getByText(/locked/i)).toHaveClass("red-led");
+  expect(getByText(/closed/i)).toHaveClass("red-led");
+});
+
+test("Use green-led for unlocked or open", () => {
+  const { getByText } = render(<Display closed={false} locked={false} />);
+
+  expect(getByText(/unlocked/i)).toHaveClass("green-led");
+  expect(getByText(/open/i)).toHaveClass("green-led");
 });
